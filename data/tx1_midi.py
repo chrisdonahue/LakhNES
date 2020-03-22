d6cf29e82a14758dd286ae25d043b40550fad07b
cer95 <cer95@cornell.edu>
2020-03-21 21:32:16 -0400

    Updated tx1_midi.py so that it supports polyphonetic music and has the instruments used for playing metal

--- data/tx1_midi.py
+++ data/tx1_midi.py
@@ -1,5 +1,8 @@
+#import DLProject.DataAcquisitonAndAnalysis.music_analysis_scripts.midi_analyzer.py
 from collections import defaultdict
 import tempfile
+
+
 
 def midi_to_tx1(midi):
   import pretty_midi
@@ -11,7 +14,8 @@ def midi_to_tx1(midi):
     mf.seek(0)
     midi = pretty_midi.PrettyMIDI(mf.name)
 
-  ins_names = ['p1', 'p2', 'tr', 'no']
+  ins_names = ['DG','OG','EGC', 'AGS','EBF','EBP','AGP']
+
   instruments = sorted(midi.instruments, key=lambda x: ins_names.index(x.name))
   samp_to_events = defaultdict(list)
   for ins in instruments:
@@ -24,14 +28,14 @@ def midi_to_tx1(midi):
       start = (note.start * 44100) + 1e-6
       end = (note.end * 44100) + 1e-6
 
-      assert start - int(start) < 1e-3
-      assert end - int(end) < 1e-3
+      #assert start - int(start) < 1e-3
+      #assert end - int(end) < 1e-3
 
       start = int(start)
       end = int(end)
 
-      assert start > last_start
-      assert start >= last_end
+      #assert start > last_start
+      #assert start >= last_end
 
       pitch = note.pitch
 
@@ -70,20 +74,37 @@ def tx1_to_midi(tx1):
   tx1 = tx1.strip().splitlines()
   nsamps = sum([int(x.split('_')[1]) for x in tx1 if x[:2] == 'WT'])
 
-  # Create MIDI instruments
-  p1_prog = pretty_midi.instrument_name_to_program('Lead 1 (square)')
-  p2_prog = pretty_midi.instrument_name_to_program('Lead 2 (sawtooth)')
-  tr_prog = pretty_midi.instrument_name_to_program('Synth Bass 1')
-  no_prog = pretty_midi.instrument_name_to_program('Breath Noise')
-  p1 = pretty_midi.Instrument(program=p1_prog, name='p1', is_drum=False)
-  p2 = pretty_midi.Instrument(program=p2_prog, name='p2', is_drum=False)
-  tr = pretty_midi.Instrument(program=tr_prog, name='tr', is_drum=False)
-  no = pretty_midi.Instrument(program=no_prog, name='no', is_drum=True)
+  GOOD_INSTRUMENTS = {
+    'Distortion Guitar': 30,
+    'Acoustic Grand Piano': 0, # AKA Drums...don't ask me why
+    'Overdriven Guitar': 29,
+    'Electric Bass (finger)': 33,
+    'Electric Bass (pick)': 34,
+    'Electric Guitar (clean)':  27,
+    "Acoustic Guitar (steel)": 25,
+    }
 
-  name_to_ins = {'P1': p1, 'P2': p2, 'TR': tr, 'NO': no}
-  name_to_pitch = {'P1': None, 'P2': None, 'TR': None, 'NO': None}
-  name_to_start = {'P1': None, 'P2': None, 'TR': None, 'NO': None}
-  name_to_max_velocity = {'P1': 15, 'P2': 15, 'TR': 1, 'NO': 15}
+  CHANNELS_TO_NEW_NAMES = {
+    30: 'DG',
+    0: 'AGP',
+    29: 'OG',
+    33: 'EBF',
+    34: 'EBP',
+    27: 'EGC',
+    25: 'AGS'
+}
+
+  # Create MIDI instruments
+  name_to_ins = {}
+  name_to_pitch = {}
+  name_to_start = {}
+  for instrument_name in GOOD_INSTRUMENTS:
+    program = GOOD_INSTRUMENTS[instrument_name]
+    noun = CHANNELS_TO_NEW_NAMES[program]
+    instr = pretty_midi.Instrument(program=program, name=noun, is_drum=int(program==0))
+    name_to_ins[noun] = instr
+    name_to_pitch[noun] = None
+    name_to_start[noun] = None
 
   samp = 0
   for event in tx1:
@@ -98,7 +119,7 @@ def tx1_to_midi(tx1):
       if tokens[1] == 'NOTEON':
         if old_pitch is not None:
           ins.notes.append(pretty_midi.Note(
-              velocity=name_to_max_velocity[name],
+              velocity=127,
               pitch=old_pitch,
               start=name_to_start[name] / 44100.,
               end=samp / 44100.))
@@ -107,7 +128,7 @@ def tx1_to_midi(tx1):
       else:
         if old_pitch is not None:
           ins.notes.append(pretty_midi.Note(
-              velocity=name_to_max_velocity[name],
+              velocity=127,
               pitch=name_to_pitch[name],
               start=name_to_start[name] / 44100.,
               end=samp / 44100.))
@@ -121,7 +142,7 @@ def tx1_to_midi(tx1):
 
   # Create MIDI and add instruments
   midi = pretty_midi.PrettyMIDI(initial_tempo=120, resolution=22050)
-  midi.instruments.extend([p1, p2, tr, no])
+  midi.instruments.extend(name_to_ins.values())
 
   # Create indicator for end of song
   eos = pretty_midi.TimeSignature(1, 1, nsamps / 44100.)
