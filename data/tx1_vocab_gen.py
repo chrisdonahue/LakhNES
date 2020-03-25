20436a543e67afd27ed6bb7d0b7c73f2440e7d96
cer95 <cer95@cornell.edu>
2020-03-25 18:02:09 -0400

    Modified tx1_vocab_gen to generate vocab for metal music. to do this, I used the min_max_pitches.json generated by running note_val_analysis on all tx1 fiels in tx1 in Data_Acquistion_And_Analysis. The vocabulary is saved aas data/vocab.txt

--- data/tx1_vocab_gen.py
+++ data/tx1_vocab_gen.py
@@ -1,3 +1,4 @@
+import json
 def quantize_wait(wait):
     wait = min(wait, 100000)
 
@@ -14,22 +15,20 @@ vocab = []
 
 # Add waits
 last_wait = None
-for i in xrange(1, 100001):
+for i in range(1, 100001):
   wait = quantize_wait(i)
   if last_wait is None or wait != last_wait:
     vocab.append('WT_{}'.format(wait))
     last_wait = wait
 
 # Add notes
-ins_to_range = {
-    'P1': [33, 108],
-    'P2': [33, 108],
-    'TR': [21, 108],
-    'NO': [1, 16]
-}
-for ins in ['P1', 'P2', 'TR', 'NO']:
+with open('min_max_pitches.json', 'r') as f:
+  ins_to_range = json.loads(f.read())
+
+for ins in ins_to_range:
   vocab.append('{}_NOTEOFF'.format(ins))
-  lo, hi = ins_to_range[ins]
+  lo = ins_to_range[ins]['min_pitch']
+  hi = ins_to_range[ins]['max_pitch']
   for n in range(lo, hi + 1):
     vocab.append('{}_NOTEON_{}'.format(ins, n))
 
